3bbb4c09e3b56b0e7ff572324600d65403ad3f05
mehedyk <mahdi.kawser@gmail.com>
2025-11-24 02:00:09 +0600

    fixing error 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/PotentialLabSite/',  // This is correct
-  
+  base: '/PotentialLabSite/',
   server: {
     host: "::",
     port: 8080,
